0cdcecc78cc7f08e2ce2c6f70d314bca9e196e5f
Khan.A <aiymankhan786@gmail.com>
2022-10-04 22:51:06 -0500

    fix dependencies 1

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "khans_vpc" {
   tags       = var.vpc_tags
 }
 resource "aws_subnet" "khans_subnet" {
-  vpc_id = aws_vpc.khans_vpc.id
+  vpc_id = var.vpc_id
   cidr_block = var.subnet_cidr
 
 }
